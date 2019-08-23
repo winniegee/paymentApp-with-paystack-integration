cfa2d543432510cc99e3e59390d02c1fc4cb8a85
Winnie <osezuahofure@gmail.com>
2019-08-23 02:09:45 +0100

    initial

---
+++ Domain.Entities/Suppliers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Suppliers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AccountNumber { get; set; }
+        public string BankName { get; set; }
+        public string AccountName { get; set; }
+        public string RecipientCode { get; set; }
+    }
+}
---
+++ Domain.Interface/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Interface
+{
+    public interface IRepository<T> where T:class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Add(T Entity);
+        void Remove(T Entity);
+        void Delete(T Entity);
+
+    }
+}
